61d3b5a0b945c0f14f0154566aae9d8306d135d8
enemrocky <craigcharltonlunga1000@gmail.com>
2024-09-10 19:06:35 -0700

    update

--- src/components/CONTEXT/LinkURLProvider.jsx
+++ src/components/CONTEXT/LinkURLProvider.jsx
@@ -1,10 +1,24 @@
 import { LinkURLContext } from "./LinkURLContext";
 import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
 
 export const LinkURLProvider = ({ children }) => {
+	useEffect(() => {
+		fetch("https://api.github.com/users/enemrocky")
+			.then((response) => response.json())
+			.then((data) => {
+				setName(data.name);
+				setAvatar(data.avatar_url);
+			});
+	}, []);
+
+	const [githubNameURL, setName] = useState("");
+	const [githubAvatarURL, setAvatar] = useState("");
+
 	const linkedInURL = "https://www.linkedin.com/in/enem-rocky-74238218a/";
 	const githubURL = "https://github.com/enemrocky";
 	const xTwitterURL = "https://x.com/Enemrocky";
+	const githubReposURL = "https://github.com/enemrocky?tab=repositories";
 
 	return (
 		<LinkURLContext.Provider
@@ -12,6 +26,9 @@ export const LinkURLProvider = ({ children }) => {
 				linkedInURL,
 				githubURL,
 				xTwitterURL,
+				githubReposURL,
+				githubAvatarURL,
+				githubNameURL,
 			}}>
 			{children}
 		</LinkURLContext.Provider>
--- src/components/PAGES/Github.jsx
+++ src/components/PAGES/Github.jsx
@@ -1,16 +1,32 @@
-// import { useState, useEffect } from "react";
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import contributions from "../../assets/contributions.png";
+import { LinkURLContext } from "../CONTEXT/LinkURLContext";
 
 //show git profile essentials and repositories
 const Github = () => {
-	// const [name, setName] = useState("");
-	// useEffect(
-	// 	() =>
-	// 		fetch("https://api.github.com/users/enemrocky")
-	// 			.then((response) => response.json())
-	// 			.then((data) => setName(data.name)),
-	// 	[]
-	// );
-	return <div>{/* github  importants */}</div>;
+	const { githubReposURL } = useContext(LinkURLContext);
+	const { githubAvatarURL } = useContext(LinkURLContext);
+	const { githubNameURL } = useContext(LinkURLContext);
+
+	return (
+		<div className="flex flex-col content-center w-4/5 mx-auto py-10 text-center">
+			<img
+				src={githubAvatarURL}
+				className="rounded-full w-80 shadow-xl mx-auto"
+			/>
+			<div className="w-80 mb-8 mt-4 capitalize mx-auto">
+				<h1 className="mb-1 font-semibold text-lg">{githubNameURL}</h1>
+				<NavLink
+					to={githubReposURL}
+					target="_blank"
+					className="text-green-500">
+					Visit my repositories
+				</NavLink>
+			</div>
+			<img src={contributions} alt="graph of github contributions" />
+		</div>
+	);
 };
 
 export default Github;
